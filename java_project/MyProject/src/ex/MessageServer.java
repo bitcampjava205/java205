7be3e5205f1c1fdf1ba674c62c68e904fe9ccd5d
bitcampjava205 <bitcamp.java205@gmail.com>
2021-06-04 16:26:00 +0900

    Message Socket
    Message Socket

--- java_project/MyProject/src/ex/MessageServer.java
+++ java_project/MyProject/src/ex/MessageServer.java
@@ -13,15 +13,18 @@ public class MessageServer {
 
 		ServerSocket ss = null;
 		Socket s = null;
-		
+
 		DataInputStream din = null;
 		DataOutputStream dout = null;
-		
-		try {
-			
-			ss = new ServerSocket(8888);
 
+		try {
+
+			ss = new ServerSocket(8888);
+			System.out.println("서버 실행");
+			System.out.println("----------------------------");
 			s = ss.accept();
+			System.out.println("client 접속");
+			System.out.println("----------------------------");
 
 			din = new DataInputStream(s.getInputStream());
 			dout = new DataOutputStream(s.getOutputStream());
@@ -29,61 +32,27 @@ public class MessageServer {
 			Scanner sc = new Scanner(System.in);
 
 			String str1 = null; // 받는 메시지
-			String str2 = null; // 보내는 메시지
+			String str2 = ""; // 보내는 메시지
 
 			while (!str2.equals("exit")) {
 
 				str1 = din.readUTF();
-				System.out.println(str1);
+				System.out.println("Client Message : " + str1);
 
 				str2 = sc.nextLine();
 				dout.writeUTF(str2);
 			}
-			
-			
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+
 		} finally {
 
-			if(din != null) {
-				try {
-					din.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			if(dout != null) {
-				try {
-					dout.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			if(s != null) {
-				try {
-					s.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-			if(ss != null) {
-				try {
-					ss.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
+			Util.close(din);
+			Util.close(dout);
+			Util.close(s);
+			Util.close(ss);
+
 		}
 
 	}
---
+++ java_project/MyProject/src/ex/Util.java
@@ -0,0 +1,51 @@
+package ex;
+
+import java.io.FilterInputStream;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Util {
+
+	public static void close(ServerSocket ss) {
+		if (ss != null) {
+			try {
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(Socket s) {
+		if (s != null) {
+			try {
+				s.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(FilterInputStream fin) {
+		if (fin != null) {
+			try {
+				fin.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(FilterOutputStream fout) {
+		if (fout != null) {
+			try {
+				fout.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
