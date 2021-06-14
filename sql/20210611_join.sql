9106bc56edbdbaaf3e7e082a7b7cdabb116fd61d
bitcampjava205 <bitcamp.java205@gmail.com>
2021-06-14 11:21:23 +0900

    Update 20210611_join.sql

--- sql/20210611_join.sql
+++ sql/20210611_join.sql
@@ -7,13 +7,49 @@
 
 select *
 from emp, dept
-where emp.deptno=dept.deptno
 ;
 
 -- ansi cross join
 select * 
 from emp cross join dept
 ;
+
+
+-- EQUI JOIN
+
+select *
+from emp, dept
+where emp.deptno=dept.deptno
+;
+
+select * from orders;
+select * from customer;
+select * from book;
+
+select o.orderid, c.name, b.bookname
+from orders o, customer c, book b
+where o.custid=c.custid and o.bookid=b.bookid
+order by o.orderid
+;
+
+-- ansi -> inner join
+-- 오라클 EQUI JOIN
+
+select *
+from emp, dept
+where emp.deptno=dept.deptno
+;
+
+select *
+from emp e inner join dept d
+on e.deptno=d.deptno
+;
+
+
+
+
+
+
 
 -- 이름이 SCOTT인 사람의 where e.ename='SCOTT'
 --  부서명을 출력해봅시다. 
@@ -22,8 +58,32 @@ from emp cross join dept
 select e.ename, d.dname , d.deptno
 from emp e, dept d
 where e.deptno = d.deptno --and ename='SCOTT'
+      and e.ename='SCOTT'
 order by d.dname, e.ename
 ;
+
+select *
+from emp e inner join dept d
+on e.deptno=d.deptno
+where e.ename='SCOTT'
+;
+
+-- 비교하는 컬럼의 이름이 같을 때
+-- on 절을 생략하고 -> using 을 이용하면 조건식을 간략하게 처리
+select *
+from emp e inner join dept d
+--on e.deptno=d.deptno
+using (deptno)
+where e.ename='SCOTT'
+;
+
+-- NATURAL JOIN
+select *
+from emp NATURAL JOIN dept
+;
+
+
+
 
 -- self JOIN : 자기 자신을 조인 ( 참조 )
 -- emp -> mgr 컬럼이 emp.empno 참조
@@ -31,6 +91,22 @@ select e.empno, e.ename, e.mgr , m.ename
 from emp e, emp m
 where  e.mgr=m.empno(+)
 ;
+
+-- outer JOIN
+select e.empno, e.ename, e.mgr , m.ename
+from emp e, emp m
+where  e.mgr=m.empno(+)  --> 표현하고자 하는 컬럼이 테이블의 반대쪽, null 값을 가지는 위치에 (+) 
+;
+
+-- [left | right | full] outer join
+select e.ename, e.mgr, m.ename as "관리자"
+--from emp e left outer join emp m
+from emp m right outer join emp e
+on e.mgr=m.empno
+;
+
+
+
 
 
 -- NON EQUI JOIN
@@ -45,7 +121,15 @@ where sal between s.losal and s.hisal
 order by e.ename
 ;
 
+-- 회원별 구매 내역출력
+select c.name--, count(o.orderid) as "구매횟수", avg(saleprice)
+from orders o , customer c
+where o.custid(+)=c.custid
+group by c.name
+order by c.name
+;
 
+select * from customer;
 
 
 
