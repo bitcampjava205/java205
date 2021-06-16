46207a9eea14f673495fbb02b7de234e8fa3c306
bitcampjava205 <bitcamp.java205@gmail.com>
2021-06-16 16:22:19 +0900

    Update ddl_phonebook.sql

--- sql/ddl_phonebook.sql
+++ sql/ddl_phonebook.sql
@@ -7,6 +7,34 @@ CREATE table phoneInfo_basic (
     fr_address varchar2(20),
     fr_regdate date default sysdate
 );
+
+-- INSERT : CREATE
+DESC phoneinfo_basic;
+insert into phoneinfo_basic
+values (1, 'KING', '010-0000-0000', 'king@gmail.com', 'KOREA', sysdate)
+;
+insert into phoneinfo_basic (idx, fr_name, fr_phonenumber)
+values (2, 'SCOTT', '010-9999-9999')
+;
+
+-- SELECT : READ
+select * from phoneinfo_basic;
+select * from phoneinfo_basic where idx=2;
+
+-- UPDATE : UPDATE
+-- SCOTT의 이메일을 업데이트, 주소도 업데이트
+-- SCOTT 의 idx -> 2
+ update phoneinfo_basic
+ set fr_email='scott@naver.com', fr_address='서울'
+ where idx=2 --fr_name='SCOTT'
+;
+
+-- DELETE : DELETE
+delete from phoneinfo_basic
+where idx=1
+;
+
+
 create table phoneinfo_univ (
     idx number(6),
     fr_u_major VARCHAR2(20) default 'N' not null,
@@ -16,6 +44,41 @@ create table phoneinfo_univ (
     constraint chk check (fr_u_year between 1 and 4), -- 테이블 레벨에서 check  제약 설정 
     constraint pi_univ_ref_FK FOREIGN KEY (fr_ref) REFERENCES phoneInfo_basic (idx)
 );
+
+-- INSERT : CREATE 데이터 생성
+-- 대학친구의 정보를 입력
+-- 1 basic 정보 입력
+insert into phoneinfo_basic
+values (3, 'SON', '010-1111-1111', 'son@gmail.com', 'KOREA', sysdate)
+;
+-- 2. univ 정보 입력
+insert into phoneinfo_univ
+values (1, 'COMPUTER', 4, 3)
+;
+
+-- SELECT : READ   데이터 검색
+select fr_name, pu.fr_u_major, pu.fr_u_year
+from phoneinfo_basic pb , phoneinfo_univ pu
+where pb.idx=pu.fr_ref
+;
+
+-- UPDATE : UPDATE 데이터 수정
+-- 전공과 학년을 수정, idx 또는 외래키
+update phoneinfo_univ
+set fr_u_major='KOR', fr_u_year=1
+where idx=1
+;
+
+-- DELETE : DELETE 데이터 삭제
+delete from phoneinfo_univ
+where idx=1
+;
+delete from phoneinfo_basic
+where idx=3
+;
+
+
+
 -- phoneinfo_com
 create table phoneinfo_com (
     idx number(6) constraint pi_com_idx_PK primary key,
@@ -23,4 +86,31 @@ create table phoneinfo_com (
     fr_ref number(6) not null constraint pi_com_ref_FK REFERENCES phoneinfo_basic (idx)
 );
 
+-- INSERT : CREATE
+insert into phoneinfo_basic
+values (4, 'PARK', '010-7777-7777', 'park@gmail.com', 'LONDON', sysdate)
+;
+insert into phoneinfo_com
+values (1, 'NAVER', 4)
+;
 
+-- SELECT : READ
+select fr_name, pb.fr_phonenumber, pb.fr_email, pb.fr_address, pc.fr_c_company
+from phoneinfo_basic pb , phoneinfo_com pc
+where pb.idx=pc.fr_ref
+;
+
+-- UPDATE : UPDATE
+-- 회사정보를 수정
+update phoneinfo_com
+set fr_c_company='GOOGLE'
+where idx=1
+;
+
+-- DELETE : DELETE
+delete from phoneinfo_com
+where idx=1
+;
+delete from phoneinfo_basic
+where idx=4
+;
