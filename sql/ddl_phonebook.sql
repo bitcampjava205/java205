03b0bb01f885ba3d9e01418647db92826ae65643
bitcampjava205 <bitcamp.java205@gmail.com>
2021-06-16 14:47:20 +0900

    Create ddl_phonebook.sql

---
+++ sql/ddl_phonebook.sql
@@ -0,0 +1,26 @@
+-- PhoneBook DDL : 테이블 정의서 를 참고 해서 DDL 장성한다.
+CREATE table phoneInfo_basic (
+    idx number(6) constraint pi_basic_idx_PK primary key,
+    fr_name VARCHAR2(20) not null,
+    fr_phonenumber varchar2(20) not null,
+    fr_email varchar2(20) ,
+    fr_address varchar2(20),
+    fr_regdate date default sysdate
+);
+create table phoneinfo_univ (
+    idx number(6),
+    fr_u_major VARCHAR2(20) default 'N' not null,
+    fr_u_year number(1) default 1  not null, --check (fr_u_year between 1 and 4),
+    fr_ref number(6) not null,
+    constraint pi_univ_idx_PK primary key (idx),
+    constraint chk check (fr_u_year between 1 and 4), -- 테이블 레벨에서 check  제약 설정 
+    constraint pi_univ_ref_FK FOREIGN KEY (fr_ref) REFERENCES phoneInfo_basic (idx)
+);
+-- phoneinfo_com
+create table phoneinfo_com (
+    idx number(6) constraint pi_com_idx_PK primary key,
+    fr_c_company VARCHAR2(20) default 'N' not null,
+    fr_ref number(6) not null constraint pi_com_ref_FK REFERENCES phoneinfo_basic (idx)
+);
+
+
