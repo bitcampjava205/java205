d7ada41d5371710d469a15afa3c100005837bbd9
bitcampjava205 <bitcamp.java205@gmail.com>
2021-06-16 14:47:16 +0900

    Update 20210616_DML.sql

--- sql/20210616_DML.sql
+++ sql/20210616_DML.sql
@@ -104,4 +104,90 @@ set sal = sal*1.1
 update emp01
 set hiredate=sysdate
 ;
+rollback;
+select * from emp01;
+-- 부서번호가 10번인 사원의 
+-- 부서번호를 30번으로 수정합시다.
+update emp01
+set deptno=30
+where deptno=10
+;
+
+
+
+-- 급여가 3000 이상인 사원만 급여를 10% 인상합시다.
+update emp01
+set sal=sal*1.1
+where sal>=3000
+;
+
+
+-- 1981년에 입사한 사원의 
+-- 입사일이 오늘로 수정합시다.
+update emp01
+set hiredate=sysdate
+where substr(hiredate, 1, 2)='81'
+;
+
+-- SCOTT 사원의 부서번호는 30번으로, 직급은 MANAGER로 한꺼번에 수정하도록 합시다.
+update emp01
+set deptno=30, job='MANAGER'
+where ename='SCOTT'
+;
+
+-- SCOTT 사원의 
+-- 입사일자는 오늘로, 급여를 50 으로 커미션을 4000 으로 수정합시다.
+update emp01
+set hiredate=sysdate, sal=50, comm=4000
+where ename='SCOTT'
+;
+select * from emp01;
+
+-- 20번 부서의 지역명을 
+-- 40번 부서의 지역명으로 변경하기 위해서 서브 쿼리문을 사용해 봅시다.
+
+truncate table dept01;
+
+insert into dept01
+select * from dept
+;
+select loc from dept01 where deptno=40;
+
+update dept01
+set loc=(select loc from dept01 where deptno=40)
+where deptno=20
+;
+
+select * from dept01;
+
+-- 서브 쿼리를 이용해서 
+-- 부서번호가 20인 부서의 부서명과 지역명을 
+-- 부서 번호가 10번인 부서와 동일하게 변경하도록 해 봅시다.
+select * from dept01 where deptno=10;
+select * from dept01 where deptno=20;
+
+update dept01
+set (dname, loc) = (select dname, loc from dept01 where deptno=10)
+where deptno=20
+;
+
+-- 데이터의 삭제 : 행단위 삭제
+-- delete from 테이블 이름 where 조건
+
+-- dept01 테이블의 모든 데이터를 삭제 하자
+delete from dept01 ;
+
+-- 이름이 SCOTT인 사원을 삭제
+delete from emp01 where ename='SCOTT';
+select * from emp01;
+
+
+-- 사원 테이블에서 부서명이 SALES인 사원을 모두 삭제해봅시다.
+select deptno from dept where dname='SALES';
+delete from emp01
+where deptno=(select deptno from dept where dname='SALES')
+;
+
+
+
 
