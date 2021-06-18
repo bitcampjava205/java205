23cb123946ac71bf55d12a0a39a3556906e5d4eb
bitcampjava205 <bitcamp.java205@gmail.com>
2021-06-18 12:26:27 +0900

    Update JDBCTest.java

--- java_project/JDBCTest/src/jdbc/JDBCTest.java
+++ java_project/JDBCTest/src/jdbc/JDBCTest.java
@@ -2,6 +2,8 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,8 +11,14 @@ public class JDBCTest {
 
 	public static void main(String[] args) {
 		
+		// 연결 객체 : 연결 정보를 가진다.
 		Connection conn = null;
+		// sql을 실행 할  메소드 제공
 		Statement stmt = null;
+		// executeQuery() 반환타입 -> select의 결과(표)를 담는 객체 
+		ResultSet rs = null;
+		//Statement -> PreparedStatement  : 성능개선
+		PreparedStatement pstmt = null;
 		
 		try {
 			// 1. 드라이버 로드
@@ -28,6 +36,50 @@ public class JDBCTest {
 			// 3. sql 처리
 			stmt = conn.createStatement();
 			
+			int dno = 10;
+			String otype = "deptno";
+			
+			String sqlSelect = 
+					"select * from dept where deptno = "+dno+" order by " + otype;
+					
+			rs = stmt.executeQuery(sqlSelect);
+			
+			// rs.next() -> 다음행의 존재 유무 확인
+			while(rs.next()) {
+				int deptno = rs.getInt("deptno");
+				System.out.print(deptno + "\t");
+				String dname = rs.getString("dname");
+				System.out.print(dname + "\t");
+				String loc = rs.getString("loc");
+				System.out.println(loc + "\t");
+			}
+			
+			/////////////////////////////////////////////
+			// PreparedStatement -> Sql 먼저 등록 -> 매개변수처럼 ?를 이용해서 나중에 변수를 바인딩
+			
+			System.out.println("PrepqredStatement 사용");
+			System.out.println("=========================================");
+			
+			String sqlSelect2 = "select * from dept where deptno = ? ";
+			pstmt = conn.prepareStatement(sqlSelect2);
+			// ? 변수에 데이터 바인딩
+			pstmt.setInt(1, 10);
+			
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				int deptno = rs.getInt("deptno");
+				System.out.print(deptno + "\t");
+				String dname = rs.getString("dname");
+				System.out.print(dname + "\t");
+				String loc = rs.getString("loc");
+				System.out.println(loc + "\t");
+			}
+			
+			
+			
+			
+			
+			
 			
 			
 			
@@ -42,6 +94,16 @@ public class JDBCTest {
 		} finally {
 			
 			// 4. close
+			
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
 			
 			if(stmt != null) {
 				try {
