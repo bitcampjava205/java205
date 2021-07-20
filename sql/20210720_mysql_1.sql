9b39f165eca5278fddc8067d405b4a80475d02f5
bitcampjava205 <bitcamp.java205@gmail.com>
2021-07-20 12:52:43 +0900

    mySql Dept Demp

---
+++ sql/20210720_mysql_1.sql
@@ -0,0 +1,16 @@
+INSERT INTO `project`.`test`
+(`memo`)
+VALUES
+('test memo2');
+
+UPDATE `project`.`test`
+SET
+`memo` = "edit mDELETE FROM `project`.`test`
+WHERE <{where_expression}>;
+mo"
+WHERE `idx` = 2;
+
+
+DELETE FROM project.test
+WHERE idx=2;
+
