f7a809e4e9a3f51d5330aa88ace0ce22a5b0bf26
bitcampjava205 <bitcamp.java205@gmail.com>
2021-07-21 12:09:00 +0900

    JSP jdbc, DAO

--- jsp_project/FIrst-Web/WebContent/jdbc/dept_editForm.jsp
+++ jsp_project/FIrst-Web/WebContent/jdbc/dept_editForm.jsp
@@ -1,3 +1,5 @@
+<%@page import="java.sql.SQLException"%>
+<%@page import="dept.dao.DeptDao"%>
 <%@page import="jdbc.util.ConnectionProvider"%>
 <%@page import="dept.domain.Dept"%>
 <%@page import="java.sql.DriverManager"%>
@@ -11,34 +13,22 @@
 	String deptno = request.getParameter("deptno");
 	//out.println(deptno);
 	
-	// 전달받은 부서번호로 부서정보를 가져온다
+	// 전달받은 부서번호로 부서정보를 가져온다 -> 처리 -> Dept -> 공유
 	// 1. 드라이버 로드 : 서블릿클래스 Loader에서 드라이버 로드
 	// 2. DB 연결
 	Connection conn = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
-
-	conn = ConnectionProvider.getConnection();
+	DeptDao dao = null;	
 	
-	Dept dept = null;
-	
-	String sqlSelect = "select * from dept where deptno=? ";
-	pstmt = conn.prepareStatement(sqlSelect);
-	pstmt.setInt(1, Integer.parseInt(deptno));
-	
-	rs = pstmt.executeQuery();
-	
-	if(rs.next()){
-		dept = new Dept();
-		dept.setDeptno(rs.getInt("deptno"));
-		dept.setDname(rs.getString("dname"));
-		dept.setLoc(rs.getString("loc"));
+	try{
+		conn = ConnectionProvider.getConnection();
+		dao = DeptDao.getInstance();
+		
+		// 부서정보를 form_view.jsp 전달(공유)
+		request.setAttribute("dept", dao.selectByDeptno(conn, Integer.parseInt(deptno)));
+		
+	} catch (SQLException e ){
+		e.printStackTrace();
 	}
-	
-	//out.println(dept);
-	
-	// 부서정보를 form_view.jsp 전달(공유)
-	request.setAttribute("dept", dept);
 	
 %>
 <jsp:forward page="form_view.jsp"/>
--- jsp_project/FIrst-Web/src/dept/dao/DeptDao.java
+++ jsp_project/FIrst-Web/src/dept/dao/DeptDao.java
@@ -8,21 +8,26 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.print.attribute.standard.PrinterIsAcceptingJobs;
+
 import dept.domain.Dept;
 import jdbc.util.JdbcUtil;
 
 public class DeptDao {
-	
+
 	// 싱글톤 패턴
 	// 1. 인스턴스 생성을 막는다!!!
-	private DeptDao() {}
+	private DeptDao() {
+	}
+
 	// 2. 클래스 내부에서 인스턴스를 생성!!
 	private static DeptDao dao = new DeptDao();
+
 	// 3. 외부에서 참조값을 반환 받을수 있는 메소드
 	public static DeptDao getInstance() {
-		return dao==null? new DeptDao() : dao  ;
+		return dao == null ? new DeptDao() : dao;
 	}
-	
+
 	public List<Dept> getDeptList(Connection conn) {
 
 		Statement stmt = null;
@@ -37,7 +42,7 @@ public class DeptDao {
 			list = new ArrayList<Dept>();
 
 			while (rs.next()) {
-				list.add(new Dept(rs.getInt(1), rs.getString(2), rs.getString(3)));
+				list.add(makeDept(rs));
 			}
 
 		} catch (SQLException e) {
@@ -49,21 +54,103 @@ public class DeptDao {
 		}
 		return list;
 	}
-	
+
 	public int insertDept(Connection conn, Dept dept) {
-		
+
 		int resultCnt = 0;
 		PreparedStatement pstmt = null;
-		
+
 		String sql = "insert into dept values (?, ?, ?)";
-		
+
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, dept.getDeptno());
 			pstmt.setString(2, dept.getDname());
 			pstmt.setString(3, dept.getLoc());
-			
+
 			resultCnt = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(pstmt);
+		}
+
+		return resultCnt;
+
+	}
+
+	public int deleteDept(Connection conn, int deptno) {
+		int resultCnt = 0;
+		PreparedStatement pstmt = null;
+
+		String sql = "delete from dept where deptno=?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, deptno);
+
+			resultCnt = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(pstmt);
+		}
+
+		return resultCnt;
+
+	}
+
+	public Dept selectByDeptno(Connection conn, int deptno) {
+
+		Dept dept = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		String sql = "select * from dept where deptno=?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, deptno);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dept = makeDept(rs);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+
+		return dept;
+
+	}
+	
+	
+	public int updateDept(Connection conn, Dept dept) {
+		
+		int resultCnt = 0;
+		
+		PreparedStatement pstmt = null;
+		
+		String sql = "update dept set dname=?, loc=? where deptno=?";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dept.getDname());
+			pstmt.setString(2, dept.getLoc());
+			pstmt.setInt(3, dept.getDeptno());
+			
+			resultCnt = pstmt.executeUpdate();	
+			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -76,28 +163,23 @@ public class DeptDao {
 		
 	}
 	
-	public int deleteDept(Connection conn, int deptno) {
-		int resultCnt = 0;
-		PreparedStatement pstmt = null;
+	private Dept makeDept(ResultSet rs) throws SQLException {
 		
-		String sql = "delete from dept where deptno=?";
+		Dept dept = new Dept();
+		dept.setDeptno(rs.getInt("deptno"));
+		dept.setDname(rs.getString("dname"));
+		dept.setLoc(rs.getString("loc"));
 		
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, deptno);
-			
-			resultCnt = pstmt.executeUpdate();
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			JdbcUtil.close(pstmt);
-		}
-		
-		return resultCnt;
+		return dept;	
 		
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
