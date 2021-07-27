d697bd97bb68a00cb8bbab26b434aea60e270d91
bitcampjava205 <bitcamp.java205@gmail.com>
2021-07-27 10:19:48 +0900

    Update 20210727_guestbook_message.sql

--- sql/20210727_guestbook_message.sql
+++ sql/20210727_guestbook_message.sql
@@ -5,3 +5,8 @@ create table project.guestbook_message (
     message text not null,
     regdate timestamp default current_timestamp
 );
+
+-- select 
+select * from project.guestbook_message order by regdate desc;  
+-- limit index, count : 페이징 처리
+select * from project.guestbook_message order by regdate desc limit 0, 3;  
