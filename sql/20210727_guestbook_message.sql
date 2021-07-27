0f05b5bfc98e29a80ebd8e5572fe63232f1b4794
bitcampjava205 <bitcamp.java205@gmail.com>
2021-07-27 10:14:15 +0900

    Create 20210727_guestbook_message.sql

---
+++ sql/20210727_guestbook_message.sql
@@ -0,0 +1,7 @@
+create table project.guestbook_message (
+	messageid int not null auto_increment primary key,
+    guestname varchar(20) not null,
+    password varchar(20) not null,
+    message text not null,
+    regdate timestamp default current_timestamp
+);
