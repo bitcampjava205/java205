38e98052d667a33f916e079e9b2c116283fca2c9
bitcampjava205 <bitcamp.java205@gmail.com>
2021-07-29 11:04:35 +0900

    MemberManager Idcheck

--- sql/20210721_member.sql
+++ sql/20210721_member.sql
@@ -14,13 +14,15 @@ create table project.member (
 -- Select
 select * from project.member;
 select * from project.member where idx=1;
+select count(*) from project.member where memberid='cool';
 select * from project.member where memberid='cool' and password='1111';
+
 
 -- insert
 INSERT INTO project.member
 (memberid, password, membername)
 VALUES
-('cool', '1111', 'COOL');
+('cool123', '1111', 'COOL');
 
 -- update
 update project.member
