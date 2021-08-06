34cd338b408f9f6444f36a98a3e55cf169b3484f
bitcampjava205 <bitcamp.java205@gmail.com>
2021-08-06 15:01:19 +0900

    OpenProject

---
+++ spring_project/OpenProject/src/main/java/com/bitcamp/op/member/dao/MemberDao.java
@@ -0,0 +1,12 @@
+package com.bitcamp.op.member.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDao {
+	
+	public void login() {
+		System.out.println("로그인합니다.");
+	}
+
+}
